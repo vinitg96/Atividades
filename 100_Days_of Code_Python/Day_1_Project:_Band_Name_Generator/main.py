cb7edb8044cfd897692bf4b60fcb4fcb425396c0
Vinicius Torres <53446126+vinitg96@users.noreply.github.com>
2022-05-10 22:16:55 -0300

    create dir 100 days of code

---
+++ 100_Days_of Code_Python/Day_1_Project:_Band_Name_Generator/main.py
@@ -0,0 +1,14 @@
+#1. Create a greeting for your program.
+print("Bem vindo ao Gerador de Nome de Banda")
+
+#2. Ask the user for the city that they grew up in.
+city = input("Qual a sua cidade natal? \n")
+
+#3. Ask the user for the name of a pet.
+pet_name = input("Qual o nome do seu animal de estimação? \n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("O nome da banda poderia ser " + city + " " + pet_name)
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
